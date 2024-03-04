9058a7f39d558eee833be9621b0d68efcf76f78a
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-04 21:17:48 +0600

    Add user management route

--- UIWebApi/src/app/app-routing.module.ts
+++ UIWebApi/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'user-management',
+    loadChildren: () =>
+      import('./user-management/user-management.module').then((m) => m.UserManagementModule),
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
