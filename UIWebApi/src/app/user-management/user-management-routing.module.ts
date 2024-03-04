554823453d96b4f2b95be15316c9baa68733c147
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-04 21:42:52 +0600

    Add user registration route to user management module

--- UIWebApi/src/app/user-management/user-management-routing.module.ts
+++ UIWebApi/src/app/user-management/user-management-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
 
 const routes: Routes = [
   {
     path: "",
     children:[{
-      path: "dashboard",
-      component: DashboardComponent
-    }
+        path: "dashboard",
+        component: DashboardComponent
+      },
+      {
+        path: "user-registration",
+        component: UserRegistrationComponent
+      }
     ]
   }
 ];
