039a68bc8309498c60ac436816deeded76ced618
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-05 21:38:22 +0600

    Add CORS middleware and update variable name for user creation

--- UIApi/server.js
+++ UIApi/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+
 
 require('./database');
 const User = require('./models/User');
@@ -8,11 +11,13 @@ const User = require('./models/User');
 const app = express();
 app.use(bodyParser.json());
 
+app.use(cors());
+
 // CREATE
 app.post('/users', async (req, res) => {
-  const { id, name, email, password } = req.body;
+  const { id, userName, email, password } = req.body;
   try {
-    const newUser = new User({ id, name, email, password });
+    const newUser = new User({ id, userName, email, password });
     await newUser.save();
     res.status(201).send(newUser);
   } catch (error) {
@@ -32,44 +37,44 @@ app.get('/users', async (req, res) => {
 
 // READ (single user by ID)
 app.get('/users/:id', async (req, res) => {
-  try {
-    const user = await User.findOne({ id: req.params.id });
-    if (!user) {
-      return res.status(404).send();
+    try {
+        const user = await User.findOne({ id: req.params.id });
+        if (!user) {
+        return res.status(404).send();
+        }
+        res.status(200).send(user);
+    } catch (error) {
+        res.status(500).send(error);
     }
-    res.status(200).send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
 });
 
 // UPDATE
 app.patch('/users/:id', async (req, res) => {
-  try {
-    const updatedUser = await User.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
-    if (!updatedUser) {
-      return res.status(404).send();
+    try {
+        const updatedUser = await User.findOneAndUpdate({ id: req.params.id }, req.body, { new: true });
+        if (!updatedUser) {
+            return res.status(404).send();
+        }
+        res.status(200).send(updatedUser);
+    } catch (error) {
+        res.status(400).send(error);
     }
-    res.status(200).send(updatedUser);
-  } catch (error) {
-    res.status(400).send(error);
-  }
 });
 
 // DELETE
 app.delete('/users/:id', async (req, res) => {
-  try {
-    const deletedUser = await User.findOneAndDelete({ id: req.params.id });
-    if (!deletedUser) {
-      return res.status(404).send();
+    try {
+        const deletedUser = await User.findOneAndDelete({ id: req.params.id });
+        if (!deletedUser) {
+        return res.status(404).send();
+        }
+        res.status(200).send(deletedUser);
+    } catch (error) {
+        res.status(500).send(error);
     }
-    res.status(200).send(deletedUser);
-  } catch (error) {
-    res.status(500).send(error);
-  }
 });
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
