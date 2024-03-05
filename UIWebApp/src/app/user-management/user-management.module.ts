7338f808380b917eed522db446d24c94d593d060
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-05 23:56:51 +0600

    Add RedOnEmptyDirective to user-management module

--- UIWebApp/src/app/user-management/user-management.module.ts
+++ UIWebApp/src/app/user-management/user-management.module.ts
@@ -5,10 +5,11 @@ import { UserManagementRoutingModule } from './user-management-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
 import { FormsModule } from '@angular/forms';
+import { RedOnEmptyDirective } from '../shared/directives/red-on-empty.directive';
 
 
 @NgModule({
-  declarations: [DashboardComponent, UserRegistrationComponent],
+  declarations: [DashboardComponent, UserRegistrationComponent,RedOnEmptyDirective],
   imports: [
     CommonModule,
     UserManagementRoutingModule,
