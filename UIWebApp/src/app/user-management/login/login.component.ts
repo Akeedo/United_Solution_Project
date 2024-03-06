8dd6e8934cd4da25a955e9ad29252fbcac931553
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-06 18:20:52 +0600

    Add OnInit interface and implement login functionality

--- UIWebApp/src/app/user-management/login/login.component.ts
+++ UIWebApp/src/app/user-management/login/login.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserModelServiceService } from '../services/user-model-service.service';
+import { NgForm } from '@angular/forms';
+import { UserDataServiceService } from '../services/user-data-service.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+
+  constructor(public modelSvc: UserModelServiceService,
+    private dataSvc: UserDataServiceService) { }
+
+  ngOnInit(): void {
+    this.modelSvc.setDefaultUser();
+  }
+
+  onSave(loginForm: NgForm) {
+    this.dataSvc.onLogin(this.modelSvc.user).subscribe({
+      next: (data) => {
+        // This function runs when the Observable emits a value (i.e., the request succeeds)
+        console.log('User saved successfully', data);
+      },
+      error: (error) => {
+        // This function runs when the Observable emits an error (i.e., the request fails)
+        console.log('Error saving user', error);
+      },
+    });
+  }
 
 }
