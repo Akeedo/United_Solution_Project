152d42552d9071fe02baaac0f656dc2ed1801c82
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-06 20:05:30 +0600

    Add login component to user management routing module

--- UIWebApp/src/app/user-management/user-management-routing.module.ts
+++ UIWebApp/src/app/user-management/user-management-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserRegistrationComponent } from './user-registration/user-registration.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   {
@@ -14,6 +15,10 @@ const routes: Routes = [
       {
         path: "user-registration",
         component: UserRegistrationComponent
+      },
+      {
+        path: "login",
+        component: LoginComponent
       }
     ]
   }
