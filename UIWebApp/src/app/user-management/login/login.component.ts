d09a3c5b628706e3215ddbec6359884cca61ae37
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-07 15:11:35 +0600

    Refactor login component validation message function

--- UIWebApp/src/app/user-management/login/login.component.ts
+++ UIWebApp/src/app/user-management/login/login.component.ts
@@ -24,31 +24,6 @@ export class LoginComponent implements OnInit{
     this.modelSvc.setDefaultUser();
   }
 
-  getValidationMessage(controlName: string, form: NgForm): string | null {
-    const control = form.controls[controlName];
-    if (control && control.touched && control.invalid) {
-      // Using type assertion to safely access properties
-      if (control.errors['required']) {
-        return 'This field is required.';
-      }
-      if (control.errors['minlength']) {
-        // Using the 'as' keyword for type assertion
-        const minLengthError = control.errors['minlength'] as {requiredLength: number};
-        return `Minimum length is ${minLengthError.requiredLength} characters.`;
-      }
-      if (control.errors['maxlength']) {
-        // Similarly for maxlength
-        const maxLengthError = control.errors['maxlength'] as {requiredLength: number};
-        return `Maximum length is ${maxLengthError.requiredLength} characters.`;
-      }
-      if (control.errors['pattern']) {
-        return 'Please enter a valid email address.';
-      }
-    }
-    return null;
-  }
-  
-  
   onSave(loginForm: NgForm) {
    try{ 
     this.authSvc.onLogin(this.modelSvc.user).subscribe({
