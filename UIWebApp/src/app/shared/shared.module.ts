4435265b2ccc01c3edd8bac48e2768c28ed63cb0
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-07 20:03:50 +0600

    Add NavbarComponent to SharedModule

--- UIWebApp/src/app/shared/shared.module.ts
+++ UIWebApp/src/app/shared/shared.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RedOnEmptyDirective } from './directives/red-on-empty.directive';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NavbarComponent
+  ],
   imports: [
     CommonModule
   ],
-  exports: []
+  exports: [NavbarComponent]
 })
 export class SharedModule { }
