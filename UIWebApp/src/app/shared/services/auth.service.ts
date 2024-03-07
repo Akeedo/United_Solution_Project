93cb15fe536cd64fc497904b3e6927c84253af5c
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-07 21:16:53 +0600

    Update auth.service.ts with new imports and methods

--- UIWebApp/src/app/shared/services/auth.service.ts
+++ UIWebApp/src/app/shared/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -15,9 +15,8 @@ export class AuthService {
   onLogin(user: any): Observable<any>{
     return this.http.post<any>(this.apiURL +'/login', user);
 }
-
-   // Example implementation
-   isLoggedIn(): boolean {
+  // Example implementation
+  isLoggedIn(): boolean {
     const token = localStorage.getItem('access_token');
     // Add more complex logic here as needed, e.g., token expiration check
     return !!token;
@@ -30,7 +29,28 @@ export class AuthService {
   // Method to log out the user
   logout() {
     localStorage.removeItem('access_token'); // Remove the token
+    localStorage.removeItem('email'); 
+    localStorage.removeItem('userName'); 
+    localStorage.removeItem('refresh_token');
     // Optionally, clear other stored user info
     this.router.navigate(['/user-management/auth/login']); // Redirect to login page
   }
+
+
+refreshToken() {
+  return this.http.post<any>(this.apiURL + '/refresh-token', {
+    'refresh_token': this.getRefreshToken()
+  }).pipe(tap((tokens: any) => {
+    this.storeJwtToken(tokens.jwt);
+  }));
+}
+  
+private getRefreshToken() {
+  return localStorage.getItem('refresh_token');
+}
+
+private storeJwtToken(jwt: string) {
+  localStorage.setItem('access_token', jwt);
+}
+
 }
