3cd84032808ca51dbf51e0da8f1affa2e9ca24dc
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-07 20:03:42 +0600

    Add NavbarComponent and SharedModule

--- UIWebApp/src/app/app.module.ts
+++ UIWebApp/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
+import { SharedModule } from './shared/shared.module';
 
 
 
@@ -13,12 +15,13 @@ import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
+    
 ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-
+    SharedModule,
     
   ],
   providers: [
