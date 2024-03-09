71637c3c06bb2e2a1874326704a54f5b28131aef
Akeed_Anjum <akeedanjumoffice@gmail.com>
2024-03-10 03:07:01 +0600

    Add SharedModule to AuthModule imports

--- UIWebApp/src/app/auth/auth.module.ts
+++ UIWebApp/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { CommonModule } from '@angular/common';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     AuthRoutingModule,
-    FormsModule
+    FormsModule,
+    SharedModule
   ]
 })
 export class AuthModule { }
